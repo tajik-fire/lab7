8fc8c942f2a86422c6d1ad53ad67d8291955afbe
tajik-fire <g.u.g.g.i.proo@gmail.com>
2025-09-18 11:36:51 +0300

    Delete Лабораторная 7/src/main/java directory

--- Лабораторная 7/src/main/java/command/ICommand.java
+++
@@ -1,9 +0,0 @@
-package command;
-
-/**
- * Интерфейс ICommand определяет методы для выполнения команд.
- */
-public interface ICommand {
-    void execute(String[] args, String username, String password);
-    String getDescription();
-}
